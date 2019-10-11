ddfc492b55e52b73d421812dd92dbf9b0781a188
duonghoangdung <dung.duonghoang94@gmail.com>
2019-10-11 17:00:58 +0700

    Updates

---
+++ precache-manifest.a2ae1d066a5a677a2ed5e3a43fafa71f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899b5876589bf563f952567147ca7b4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e418ba6b8a1d35e6d7",
+    "url": "/static/css/main.acbe0b60.chunk.css"
+  },
+  {
+    "revision": "d70abe6263574521a7a3",
+    "url": "/static/js/2.db23fab7.chunk.js"
+  },
+  {
+    "revision": "01e418ba6b8a1d35e6d7",
+    "url": "/static/js/main.0e0155de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
